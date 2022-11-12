321024b0862c8b6ad6cf69ebdd09fae8fd755ecc
surendarradnerus <surendarshankar333@gmail.com>
2022-11-12 19:37:35 +0530

    validation

--- script.js
+++ script.js
@@ -10,6 +10,106 @@ const password = document.getElementById('password')
 const email = document.getElementById('email')
 const submitBtn = document.getElementById('submit')
 
+// function isEmail(email) {
+// 	return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(email);
+// }
+
+function isName(fName) {
+    return /^[A-Za-z]+$/.test(fName);
+}
+function isSurName(surName) {
+  return /^[A-Za-z]+$/.test(surName);
+}
+
+
+
+//...................validation...........................................
+
+
+function checkFname() {
+
+  const fNameValue = fName.value.trim();
+  if(fNameValue === '' || (!isName(fNameValue))) {
+    fName.style.borderColor = 'red';
+    formName.classList.add('fnerror')
+       return false;
+  } else {
+    fName.style.borderColor = 'green';
+    formName.classList.remove('fnerror')
+  }
+}
+
+function checkSname() {
+  const surNameValue = surName.value.trim();
+  if(surNameValue === '' || (!isSurName(surNameValue))) {
+    surName.style.borderColor = 'red';
+    formSname.classList.add('snerror')
+        return false;
+   }else {
+
+     surName.style.borderColor = 'green';
+     formSname.classList.remove('snerror')
+   }
+}
+
+function checkEmail() {
+  const emailValue = email.value.trim();
+  var Emformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(emailValue.match(Emformat)) {
+    email.style.borderColor = 'green';
+    formEmail.classList.remove('emailerror')
+       return true;
+  }else {
+    email.style.borderColor = 'red';
+      formEmail.classList.add('emailerror') 
+      return false;
+  } 
+}
+
+function checkPnumber() {
+
+  var Mpattern =/^\d{10}$/;
+  const mobileNumberValue = mobileNumber.value.trim();
+  if (mobileNumberValue .match(Mpattern)){
+    mobileNumber.style.borderColor = 'green';
+    formMobile.classList.remove('pherror')
+       return true;
+   }else {
+    mobileNumber.style.borderColor = 'red';
+    formMobile.classList.add('pherror')
+    return false;
+   }
+}
+
+function CheckPassword() {
+  const passwordValue = password.value.trim();
+  if (passwordValue .length <= 6 ){
+    password.style.borderColor = 'red';
+    formPassword.classList.add('passerror')
+       return false;
+   }else {
+    password.style.borderColor = 'green';
+    formPassword.classList.remove('passerror')
+   }
+}
+
+function checkInputs() {
+  checkFname();
+  checkSname();
+  checkEmail();
+  checkPnumber();
+  CheckPassword();
+
+}
+
+
+
+
+
+
+
+
+
 
 // form.addEventListener('submit', e => {
 // 	e.preventDefault();
@@ -25,79 +125,66 @@ const submitBtn = document.getElementById('submit')
 
 
 
-let popUp = document.getElementsByClassName('.fnerror');
-  let style = window.getComputedStyle(popUp,'::before');
-  console.log(style.visibility);
-
-  let error = style.getPropertyValue('visibility') == 'visible'
-
-function checkInputs() {
-  const fNameValue = fName.value.trim();
-  const surNameValue = surName.value.trim();
-  const mobileNumberValue = mobileNumber.value.trim();
-  const passwordValue = password.value.trim();
-  const emailValue = email.value.trim();
+// function checkInputs() {
+//   const fNameValue = fName.value.trim();
+//   const surNameValue = surName.value.trim();
+//   const mobileNumberValue = mobileNumber.value.trim();
+//   const passwordValue = password.value.trim();
+//   const emailValue = email.value.trim();
 
   
-if(fNameValue === '' || (!isName(fNameValue))) {
-  fName.style.borderColor = 'red';
-  formName.classList.add('fnerror')
-     return false;
-} else {
-  fName.style.borderColor = 'green';
-  formName.classList.remove('fnerror')
-}
+// if(fNameValue === '' || (!isName(fNameValue))) {
+//   fName.style.borderColor = 'red';
+//   formName.classList.add('fnerror')
+//      return false;
+// } else {
+//   fName.style.borderColor = 'green';
+//   formName.classList.remove('fnerror')
+// }
+// // else if (!isName(fNameValue)){
+// //   fName.style.borderColor = 'red';
+// //     return false;
+// // }console.log(fNameValue);
 
-if(surNameValue === '' || (!isSurName(surNameValue))) {
- surName.style.borderColor = 'red';
- formSname.classList.add('snerror')
-     return false;
-}else {
-  surName.style.borderColor = 'green';
-  formSname.classList.remove('snerror')
-}
+// if(surNameValue === '' || (!isSurName(surNameValue))) {
+//  surName.style.borderColor = 'red';
+//  formSname.classList.add('snerror')
+//      return false;
+// }else {
+//   surName.style.borderColor = 'green';
+//   formSname.classList.remove('snerror')
+// }
 
-if(emailValue === '' || (!isEmail(email))) {
-  email.style.borderColor = 'red';
-  formEmail.classList.add('emailerror')
-     return false;
-} else  {
-  email.style.borderColor = 'green';
-    formEmail.classList.remove('emailerror') 
-} 
+// if(emailValue === '' || (!isEmail(email))) {
+//   email.style.borderColor = 'red';
+//   formEmail.classList.add('emailerror')
+//      return false;
+// }else {
+//   email.style.borderColor = 'green';
+//     formEmail.classList.remove('emailerror') 
+// } 
 
-if(mobileNumberValue === '' ) {
-  mobileNumber.style.borderColor = 'red';
-  formMobile.classList.add('pherror')
-     return false;
-} else{
-  mobileNumber.style.borderColor = 'green';
-  formMobile.classList.remove('pherror')
-}
+// if(mobileNumberValue === '' ) {
+//   mobileNumber.style.borderColor = 'red';
+//   formMobile.classList.add('pherror')
+//      return false;
+// } else{
+//   mobileNumber.style.borderColor = 'green';
+//   formMobile.classList.remove('pherror')
+// }
 
-if (passwordValue .length <= 6){
-  password.style.borderColor = 'red';
-  formMobile.classList.add('passerror')
-     return false;
- }else {
-  password.style.borderColor = 'green';
-  formMobile.classList.remove('passerror')
- }
+// if (passwordValue === ''){
+//   password.style.borderColor = 'red';
+//   formMobile.classList.add('passerror')
+//      return false;
+//  }else {
+//   password.style.borderColor = 'green';
+//   formMobile.classList.remove('passerror')
+//  }
+  
 
+// }
 
-
-}
-
-function isEmail(email) {
-	return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(email);
-}
-
-function isName(fName) {
-    return /^[A-Za-z]+$/.test(fName);
-}
-function isSurName(surName) {
-  return /^[A-Za-z]+$/.test(surName);
-}
 
 
 
